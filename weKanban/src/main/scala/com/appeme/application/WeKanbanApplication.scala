2a03f37299109d89a6cec04b90ba8adab4faf2d6
WangWei <hhywangwei@gmail.com>
2014-11-05 23:24:52 +0800

    edit WeKanbanApplication

--- weKanban/src/main/scala/com/appeme/application/WeKanbanApplication.scala
+++ weKanban/src/main/scala/com/appeme/application/WeKanbanApplication.scala
@@ -1,5 +1,9 @@
 package com.appeme.application
 
+import com.appeme.views.CreateStory
+
+import scalaz._
+import Scalaz._
 import scalaz.http._
 import response._
 import request._
@@ -7,11 +11,25 @@ import servlet._
 import Slinky._
 
 final class WeKanbanApplication extends StreamStreamServletApplication {
+  import Request._
+
+  implicit val charset = UTF8
+
+  def param(name :String) (implicit request:Request[Stream]) =
+    (request ! name).getOrElse(List[Char]()).mkString("")
+
+  def handler(implicit request:Request[Stream], servletRequest:HttpServletRequest): Option[Response[Stream]] = {
+    request match {
+      case MethodParts(GET, "card" :: "create" :: Nil) =>
+        Some(OK(ContentType, "text/html") << strict << CreateStory(param("message")))
+      case _ => None
+    }
+  }
 
   val application = new ServletApplication[Stream, Stream] {
     def application(implicit servlet: HttpServlet, servletRequest: HttpServletRequest, request: Request[Stream]) = {
       def found(x: Iterator[Byte]) : Response[Stream] = OK << x.toStream
-      HttpServlet.resource(found, NotFound.xhtml)
+      handler | HttpServlet.resource(found, NotFound.xhtml)
     }
   }
 }
